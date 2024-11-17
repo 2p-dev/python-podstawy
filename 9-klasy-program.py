8950800aabe4b9e5c56958e02a1032ff208c7062
2p-dev <187512064+2p-dev@users.noreply.github.com>
2024-11-17 12:18:36 +0100

    Add files via upload

---
+++ 9-klasy-program.py
@@ -0,0 +1,4 @@
+# utworzyć strukturę klas zawierającą figury geometyczne: figura (klasa bazowa), prostokąt, kwadrat (to też prostokąt), koło
+# dla każdej figury podać wymagane parametry - prostokąta: długość, szerokość, kwadratu: długosć, koła: promień
+# każda z klas powinna implementować metody zwracające wartości: oblicz_obwod(), oblicz_powierzchnie()
+# dla kwadratu skorzystać z obliczania obwodu oraz powierzchni klasy bazowej (prostokąta), podając bok kwadratu 2 razu
