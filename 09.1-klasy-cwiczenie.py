4318b09db9ca9c6fcadf05c327a0dcb428592075
2p-dev <187512064+2p-dev@users.noreply.github.com>
2024-11-22 18:49:37 +0100

    Update 09.1-klasy-cwiczenie.py

--- 09.1-klasy-cwiczenie.py
+++ 09.1-klasy-cwiczenie.py
@@ -1,4 +1,6 @@
 # utworzyć strukturę klas zawierającą figury geometryczne: figura (klasa bazowa), prostokąt, kwadrat (to też prostokąt), koło
 # dla każdej figury podać wymagane parametry - prostokąta: długość, szerokość, kwadratu: długość, koła: promień
 # każda z klas powinna implementować metody zwracające wartości: oblicz_obwod(), oblicz_powierzchnie()
-# dla kwadratu skorzystać z obliczania obwodu oraz powierzchni klasy bazowej (prostokąta), podając bok kwadratu 2 razu
+# dla kwadratu skorzystać z obliczania obwodu oraz powierzchni klasy bazowej (prostokąta), podając bok kwadratu 2 razy
+
+# https://prod.liveshare.vsengsaas.visualstudio.com/join?62D628A1D25E9E4A5C00884C53D28EFE75AF
